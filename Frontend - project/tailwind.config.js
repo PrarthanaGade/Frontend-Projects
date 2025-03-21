4bd477831a1c0ebed146908dda084a16652fba9d
GeniusPrarthana <Prarthanagade25@gmail.com>
2025-03-22 03:26:25 +0530

    Added project files

---
+++ Frontend - project/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}", // Include all source files
+    ],
+    darkMode: 'class', // Enable 'class' or 'media' for dark mode
+    theme: {
+      extend: {}, // Customize your theme here
+    },
+    plugins: [], // Add any Tailwind plugins here
+  };
+  
